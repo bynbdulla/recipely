e4a872ce8184926f06875ea9e7f0d2c8299ef13d
bynbdulla <b.markhoos@gmail.com>
2026-07-20 19:09:36 +0300

    facing a problem! server is not working

--- server.js
+++ server.js
@@ -7,16 +7,14 @@ const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
-const session = require('express-session')
-const { MongoStore } = require('connect-mongo')
+const session = require("express-session");
+const { MongoStore } = require("connect-mongo");
 
-
-const authCtrl = require('./controllers/auth')
-const recipeCtrl = require('./controllers/recipes')
+const authCtrl = require("./controllers/auth");
+const recipeCtrl = require("./controllers/recipes");
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
-
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -29,44 +27,47 @@ app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI
+      mongoUrl: process.env.MONGODB_URI,
     }),
-}))
+  }),
+);
 
-app.get('/', (req, res) => {
-    res.render('home.ejs', {
-        user: req.session.user,
-    })
-})
+app.get("/", (req, res) => {
+  res.render("home.ejs", {
+    user: req.session.user,
+  });
+});
 
-app.get('/auth/sign-up', authCtrl.showSignUpForm )
-app.post('/auth/sign-up', authCtrl.signUp)
-app.get('/auth/sign-in', authCtrl.showSignInForm)
-app.post('/auth/sign-in', authCtrl.signIn)
-app.delete('/auth/sign-out', authCtrl.signOut)
+app.get("/auth/sign-up", authCtrl.showSignUpForm);
+app.post("/auth/sign-up", authCtrl.signUp);
+app.get("/auth/sign-in", authCtrl.showSignInForm);
+app.post("/auth/sign-in", authCtrl.signIn);
+app.delete("/auth/sign-out", authCtrl.signOut);
 
-app.get("/recipes/new", recipeCtrl.showNewForm)
-app.post("/recipes", recipeCtrl.create)
-app.get('/recipes', recipeCtrl.index)
-app.get('/recipes/:recipeId', recipeCtrl.show)
+app.get("/recipes/new", recipeCtrl.showNewForm);
+app.post("/recipes", recipeCtrl.create);
+app.get("/recipes", recipeCtrl.index);
+app.get("/recipes/:recipeId/edit", recipeCtrl.edit);
 
+app.put("/recipes/:recipeId", recipeCtrl.update,
+);
 
-app.get('/dashboard', async (req, res) => {
-    if (!req.session.user){
-        return res.redirect('/auth/sign-in')
-    }
-    res.render('dashboard.ejs', {
-        user: req.session.user
-    })
-})
-
+app.get("/dashboard", async (req, res) => {
+  if (!req.session.user) {
+    return res.redirect("/auth/sign-in");
+  }
+  res.render("dashboard.ejs", {
+    user: req.session.user,
+  });
+});
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
